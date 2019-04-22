87b1e89c05f0116fab7353737067b73a9709187e
Álex Porto <alexportoferreira@hotmail.com>
2019-04-22 18:29:08 -0300

    Add and setting nunjucks

--- index.js
+++ index.js
@@ -1,19 +1,22 @@
 const express = require('express')
+const nunjucks = require('nunjucks')
 
 const app = express();
+
+nunjucks.configure('views', {
+    autoescape: true,
+    express: app,
+    watch: true,
+});
+
+app.set('view engine', 'njk')
 
 app.get('/', (req, res) => {
     return res.send(`Bem-vindo,${req.query.name}`) // query params
 });
 
 app.get('/nome/:name', (req, res) => {
-    return res.send(`Bem-vindo, ${req.params.name}`); //no req estao todos os parametros e como esse parametro vem da url sem nome,usa o params e o nome do parametro
-});
-
-app.get('/cidade/:city', (req, res) => {
-   return res.json({ //tipo de resposta mt utilizada q serve qndo estamos criando API rest(sem parte de visualização)
-       message: `Your city is ${req.params.city}`
-   });
+    return res.send(`Bem-vindo`); //no req estao todos os parametros e como esse parametro vem da url sem nome,usa o params e o nome do parametro
 });
 
 app.listen(3000);
